bd816b39ba54529a2caaec3ac03206dfb8381d3b
AkashSinha007 <akashkumarsinha007@gmail.com>
2021-04-13 12:04:38 +0530

    Wrong behaviour due to no callback use on asynchronous

--- Async_Patterns/server/routes/clothing.js
+++ Async_Patterns/server/routes/clothing.js
@@ -7,18 +7,22 @@ const router = express.Router();
 router.route('/')
   .get(function(req, res) {
 
-    fs.readFile(datafile,'utf8',(err,data)=>{
-      if(err){
-        console.log(err);
-      }
-      else
-      {
-        let clothingData = JSON.parse(data);
-        console.log('Returning clothing data');
-        res.send(clothingData)
-      }
-    })
+    console.log('Returning clothing data');
+    res.send(clothingData);
     console.log('Doing more work');
   });
+
+function getClothingData(){
+  fs.readFile(datafile,'utf8',(err,data)=>{
+    if(err){
+      console.log(err);
+    }
+    else
+    {
+      let clothingData = JSON.parse(data);
+      return clothingData;
+    }
+  });
+}
 
 module.exports = router;
