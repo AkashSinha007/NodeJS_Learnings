18ca07c87b8f9a22237793ebefc2a7e664216b4a
AkashSinha007 <akashkumarsinha007@gmail.com>
2021-04-13 16:25:16 +0530

    Adding the Worker thread implementation

--- WorkerThreads/thread.js
+++ WorkerThreads/thread.js
@@ -1,13 +1,23 @@
 
-console.log('Starting the main thread');
+const {Worker, isMainThread, parentPort} = require('worker_threads');
 
-console.log('Getting Started');
-wasteTime(2000);
-console.log('In the middle');
-wasteTime(2000);
-console.log('All done');
+if(isMainThread){
+    console.log('Starting the main thread');
 
-console.log('Still in the main thread');
+    const worker= new Worker(__filename);
+    worker.on('message',(msg)=>{
+        console.log(`Worker:${msg}`);
+    });
+
+    console.log('Still in the main thread');
+}
+else {
+    parentPort.postMessage('Getting started');
+    wasteTime(2000);
+    parentPort.postMessage('In the middle');
+    wasteTime(2000);
+    parentPort.postMessage('All done');
+}
 
 function wasteTime(delay)
 {
